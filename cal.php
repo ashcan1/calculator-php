b0fce337d09721f0772c9029e7ac212b3bcc25df
Ashkan <Ashki444@gmail.com>
2021-02-12 20:55:56 +0000

    cal

---
+++ cal.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Calculator</title>
+    </head>
+    <body>
+
+
+         <form method="post">
+            <input type="text" name="numb1">
+            <input type="text" name="numb2">
+            <select name="operator" id="">
+               <option>None</option>
+               <option>Add</option>
+               <option>Subtract</option>
+               <option>Multiply</option>
+               <option>Divide</option>
+               <option>Square</option>
+            </select>
+            <button type="submit" name="submit" value="submit">Calculate</button>
+         </form>
+
+
+
+        <?php 
+
+            if (isset($_POST['submit'])) {
+                $result1 = $_POST['numb1'];
+                $result2 = $_POST['numb2'];
+                $operator = $_POST['operator'];
+                switch ($operator) {
+                    case 'None':
+                        echo "Error";
+                        break;
+                    case 'Add':
+                        echo $result1 + $result2;
+                        break;
+                    case 'Subtract':
+                        echo $result1 - $result2;
+                        break;
+                    case 'Multiply':
+                        echo $result1 * $result2;
+                        break;
+                    case 'Divide':
+                        echo $result1 / $result2;
+                        break;
+                    case 'Square':
+                        echo $result1 ** $result2;
+                        break;
+                }
+            }
+
+
+         ?>
+  
+
+    </body>
+    </html>
